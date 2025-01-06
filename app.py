5d81133a6a08b71842926a106a2bedf4619fec37
artur <git-ar.houze@aleeas.com>
2025-01-06 15:12:53 +0100

    Add: First UI interface
    Signed-off-by: artur <git-ar.houze@aleeas.com>

--- app.py
+++ app.py
@@ -9,4 +9,69 @@ Notes:
     else -> error
 - output should be excel file (downloadable)
 """
+import pandas as pd
+import streamlit as st
+
 from libraries.iw_handler import IWHandler
+
+@st.cache_data
+def convert_df(df):
+    return df.to_csv().encode("utf-8")
+
+if 'handler' not in st.session_state:
+    st.session_state['mainF'] = None
+    st.session_state['extF'] = None
+
+# do not create in session to make config editing easier
+handler = IWHandler()
+
+st.title("IW Group Creator")
+st.markdown("""
+    In the following please provide the following two things:
+    1. The main excel file containing the user choices
+    2. The file containing info about buddy group assignment
+    """)
+
+main_file = st.file_uploader("Main file", type=["xlsx", "xls"])
+help_file = st.file_uploader("Extra file (buddy group info)", type=["xlsx", "xls"])
+
+inputExistent = False
+output_df = pd.DataFrame()
+downloadDisabled = True
+
+if st.button("Generate groups"):
+    # check if input was provided
+    if main_file is None or help_file is None:
+        # if no input was provided check if previous session input exists
+        if st.session_state['mainF'] is None or st.session_state['extF'] is None:
+            st.error("Input missing", icon="🚨")
+        else:
+            st.warning('Using previous data from this session', icon="⚠️")
+            inputExistent = True
+    else:
+        # save to session state
+        st.session_state['mainF'] = main_file
+        st.session_state['extF'] = help_file
+        inputExistent = True
+
+if inputExistent:
+    handler.load_data(st.session_state['mainF'], st.session_state['extF'])
+    output_df = handler.compute()
+    # Replace with progress bar
+    downloadDisabled=False
+    st.success("Loaded successfully", icon="✅")
+
+st.divider()
+st.write("### Output:")
+st.download_button(
+    label="Download result ",
+    data=convert_df(output_df),
+    file_name="IW_Groups.csv",
+    mime="text/csv",
+    disabled=downloadDisabled
+)
+if not downloadDisabled:
+    st.write(output_df)
+
+st.divider()
+st.write("> ©️ 2024 - AR")
