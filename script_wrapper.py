2a28e2ba9f0dec40c91f14d0238c40e18743b576
artur <git-ar.houze@aleeas.com>
2025-01-22 21:46:27 +0100

    Add: wrapper for direct execution, incl. buddy group ordering output
    Signed-off-by: artur <git-ar.houze@aleeas.com>

---
+++ script_wrapper.py
@@ -0,0 +1,47 @@
+"""
+This is an alternative way of running
+the libraries/iw_handler module
+
+app.py targets a webapp, where this can be run directly from
+a laptop without the need of a web app
+"""
+import os
+
+from libraries.iw_handler import IWHandler
+
+###################################
+# CONFIGURE THIS
+# path to the input file
+INPUT_DATA = "data/data1.xlsx"
+
+# name of the output file (should end with .xlsx)
+OUTPUT_NAME = "groups.xlsx"
+###################################
+
+def main(input_data: str, ext_input: str, outpath: str):
+    """
+    The main entry point
+    Arguments:
+    - inputData (str): path to the input file
+    """
+    if not os.path.isfile(input_data):
+        raise FileNotFoundError(f"Provided path is :'{input_data}'")
+    handler = IWHandler()
+    handler.load_data(input_data, ext_input)
+    print("Loaded data successfully")
+    print("Started the process for group creation...")
+    df = handler.compute()
+    print("Created groups successfully")
+
+    df.to_excel(outpath)
+    print(f"Saved overall overview to '{outpath}'")
+
+    # save also sorted by buddy group
+    df = df.sort_values(by=["buddy group"])
+    outpath = f"{outpath.split('.')[0]}-BUDDYGROUPS.xlsx"
+    df.to_excel(outpath)
+    print(f"Saved overview sorted by buddy groups to '{outpath}'")
+
+
+if __name__ == "__main__":
+    main(INPUT_DATA, "data/data2.xlsx", OUTPUT_NAME)
