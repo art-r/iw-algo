3742dc42f6b72344c34a26c02120321f0074003d
artur <git-ar.houze@aleeas.com>
2025-01-23 20:52:27 +0100

    Add: logic for extracting buddy info
    Signed-off-by: artur <git-ar.houze@aleeas.com>

---
+++ extract_buddy_groups.py
@@ -0,0 +1,54 @@
+"""
+This script creates a folder structure
+that will then contain a per buddy group information
+"""
+import os
+import sys
+
+import pandas as pd
+###################################
+# CONFIGURE THIS
+# name of the input file that contains all groups (will be updated)
+GROUPS_FILE = "groups.xlsx"
+
+# Name of the output dir
+OUTPUT_DIR = "buddy_group_info"
+###################################
+
+
+def main(group_path, out_dir):
+    """
+    The main script
+    """
+    if not os.path.isfile(group_path):
+        raise FileNotFoundError(f"Given path is: {group_path}")
+
+    if os.path.isdir(out_dir):
+        print("WARNING: Output dir already exists!!")
+        print("Will abort")
+        sys.exit(0)
+
+    # create root dir
+    os.makedirs(out_dir)
+
+    # read data
+    df = pd.read_excel(group_path, header=0)
+
+    # sort by buddy group and reset index
+    df.sort_values("buddy group", inplace=True)
+    df.reset_index(inplace=True)
+
+    # get the unique buddy group values
+    buddy_groups = df["buddy group"].unique()
+
+    # save the per buddy group info
+    for group in buddy_groups:
+        # create buddy group dir
+        os.makedirs(os.path.join(out_dir, group))
+        # save data
+        file_path = os.path.join(out_dir, group, f"{group}-workshop.xlsx")
+        df[df["buddy group"] == group].to_excel(file_path)
+
+
+if __name__ == "__main__":
+    main(GROUPS_FILE, OUTPUT_DIR)
